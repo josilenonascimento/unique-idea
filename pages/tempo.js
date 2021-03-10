d08816c6bd656fe714f1c5a9e98eaceeee2ec488
Josileno Nascimento <josileno2015@gmail.com>
2021-03-10 16:44:09 -0300

    adicionado revalidate

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,19 +1,15 @@
 function Tempo(props) {
-  console.log('-> passando pelo front-end')
   const dynamicDate = new Date()
   const dynamicDateString = dynamicDate.toGMTString()
   return (
     <div>
       <div>{dynamicDateString} (dinâmico)</div>
-      <div>{props.staticDateString} (estático - com delay)</div>
+      <div>{props.staticDateString} (estático)</div>
     </div>
   )
 }
 
 export async function getStaticProps() {
-  console.log('-> passando pelo getStaticProps')
-  console.log('-> adicionando delay de 5 segundos')
-  await delay(5000)
   const staticDate = new Date()
   const staticDateString = staticDate.toGMTString()
 
@@ -24,7 +20,5 @@ export async function getStaticProps() {
     revalidate: 1
   }
 }
-
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 export default Tempo
